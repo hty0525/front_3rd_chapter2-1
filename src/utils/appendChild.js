bdca2125265661d28777736eab10db61fc3a9a6c
taeyoung <terry.hwang@h2ohospitality.io>
2024-10-15 12:02:25 +0900

    appendChild 자식요소 배열 아니어도 동작하게 수정

--- src/utils/appendChild.js
+++ src/utils/appendChild.js
@@ -1,9 +1,11 @@
 export function appendChild({ parent, children }) {
-  if (Array.isArray(children)) {
-    children.forEach((child) => {
-      parent.appendChild(child);
-    });
-  } else {
-    throw new Error("자식요소는 배열이어야 합니다!!!");
-  }
+	if (!parent || !children) throw new Error('부모요소와 자식요소는 있어야 합니다.');
+
+	if (parent instanceof HTMLElement === false) throw new Error('부모요소는 HTMLElement이어야 합니다.');
+
+	const childrenAry = Array.isArray(children) ? children : [children];
+	childrenAry.forEach((child) => {
+		if (parent instanceof HTMLElement === false) throw new Error('자식요소는 HTMLElement이어야 합니다.');
+		parent.appendChild(child);
+	});
 }

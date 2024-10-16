597a15e1e6b58837fea5f344478e310d3dde47f4
taeyoung <terry.hwang@h2ohospitality.io>
2024-10-16 16:03:23 +0900

    eslint function return off

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -3,7 +3,7 @@ module.exports = [
 		ignores: ['node_modules/**'], // 특정 파일 또는 폴더를 무시할 수 있음
 	},
 	{
-		files: ['*.js', '*.jsx'], // ESLint가 검사할 파일 확장자 지정
+		files: ['**/*.{js,ts,tsx}'], // ESLint가 검사할 파일 확장자 지정
 		languageOptions: {
 			ecmaVersion: 'latest', // 최신 ECMAScript 버전 사용
 			sourceType: 'module', // ES 모듈을 지원하도록 설정
@@ -12,7 +12,7 @@ module.exports = [
 			'no-console': 'error', // console.log 사용 금지
 			'no-use-before-define': ['error', { functions: false }], // 함수 선언 전 사용 금지
 			'comma-dangle': ['error', 'always-multiline'], // 마지막 요소 뒤에 쉼표 사용
-			indent: ['error', tab], // 들여쓰기 2칸
+			indent: ['error', 'tab'], // 들여쓰기 2칸
 			quotes: ['error', 'single'], // 싱글 쿼테이션 사용
 			'eol-last': ['error', 'always'], // 파일 끝에 개행 문자 추가
 			'no-trailing-spaces': 'error', // 빈 줄 끝에 공백 금지
@@ -23,7 +23,7 @@ module.exports = [
 			'no-var': 'error', // var 사용 금지
 			'max-len': ['error', { code: 120 }], // 코드 한 줄 길이 제한
 			'no-shadow': 'error', // 변수 중복 선언 금지
-			'consistent-return': 'error', // 일관된 return 사용
+			'consistent-return': 'off', // 일관된 return 사용
 			'arrow-body-style': ['error', 'as-needed'], // 필요할 때만 중괄호 사용
 			'no-duplicate-imports': 'error', // 중복된 import 금지
 			'prefer-template': 'error', // 문자열 연결 시 템플릿 리터럴 사용 권장

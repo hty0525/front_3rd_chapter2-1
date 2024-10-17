96ff15b11b7a56e926e2c165d09cf64b48c75145
terry.hwang <terry.hwang@h2ohospitality.io>
2024-10-18 02:52:06 +0900

    매직 넘버

---
+++ src/advanced/src/contant/index.ts
@@ -0,0 +1,13 @@
+export const PRODUCT_DISCOUNT_RATES = {
+	p1: 0.1,
+	p2: 0.15,
+	p3: 0.2,
+};
+
+export const PRODUCT_DISCOUNT_QUANTITY = 10;
+
+export const ALL_DISCOUNT_QUANTITY = 30;
+export const ALL_DISCOUNT_RATE = 0.25;
+
+export const TUESDAY_DISCOUNT_RATE = 0.1;
+export const REWARD_POINT_RATE = 0.001;

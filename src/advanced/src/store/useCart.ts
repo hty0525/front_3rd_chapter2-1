5e7f786e801518c8a8f8bfe01636a90301a78641
terry.hwang <terry.hwang@h2ohospitality.io>
2024-10-18 03:25:18 +0900

    추천 상품, 럭키 상품 추가

--- src/advanced/src/store/useCart.ts
+++ src/advanced/src/store/useCart.ts
@@ -13,11 +13,11 @@ export type Product = {
 };
 
 const initProducts: Product[] = [
-	{ id: 'p1', name: '상품1', price: 10000, quantity: 50, currentCartItemQuantity: 0 },
-	{ id: 'p2', name: '상품2', price: 20000, quantity: 30, currentCartItemQuantity: 0 },
-	{ id: 'p3', name: '상품3', price: 30000, quantity: 20, currentCartItemQuantity: 0 },
+	{ id: 'p1', name: '상품1', price: 10000, quantity: 50, currentCartItemQuantity: 5 },
+	{ id: 'p2', name: '상품2', price: 20000, quantity: 30, currentCartItemQuantity: 5 },
+	{ id: 'p3', name: '상품3', price: 30000, quantity: 20, currentCartItemQuantity: 5 },
 	{ id: 'p4', name: '상품4', price: 15000, quantity: 0, currentCartItemQuantity: 0 },
-	{ id: 'p5', name: '상품5', price: 25000, quantity: 10, currentCartItemQuantity: 0 },
+	{ id: 'p5', name: '상품5', price: 25000, quantity: 10, currentCartItemQuantity: 5 },
 ];
 
 export type State = {
@@ -31,10 +31,12 @@ export type Action =
 			type: 'CHANGE_CART_ITEM_QUANTITY';
 			payload: { productId: ProductId; quantity: number };
 	  }
-	| { type: 'UPDATE_TOTAL_PRICE'; payload: { discountRate: number } };
+	| { type: 'UPDATE_TOTAL_PRICE'; payload: { discountRate: number } }
+	| { type: 'LUCKY_PRODUCT_ITEM' }
+	| { type: 'SUGGEST_PRODUCT_ITEM' };
 
 function reducer(state: State, action: Action): State {
-	const { products } = state;
+	const { products, lastSelectedProductId } = state;
 
 	switch (action.type) {
 		case 'ADD_CART_ITEM': {
@@ -72,7 +74,7 @@ function reducer(state: State, action: Action): State {
 			};
 		}
 
-		case 'CHANGE_CART_ITEM_QUANTITY':
+		case 'CHANGE_CART_ITEM_QUANTITY': {
 			const { productId, quantity } = action.payload;
 
 			return {
@@ -90,7 +92,42 @@ function reducer(state: State, action: Action): State {
 				}),
 				lastSelectedProductId: productId,
 			};
+		}
 
+		case 'LUCKY_PRODUCT_ITEM': {
+			const luckyItem = products[Math.floor(Math.random() * products.length)];
+			const { quantity, price, id, name } = luckyItem;
+
+			if (Math.random() < 0.3 && quantity > 0) {
+				const luckyPrice = Math.round(price * 0.8);
+				alert(`번개세일! ${name}이(가) 20% 할인 중입니다!`);
+
+				return {
+					...state,
+					products: products.map((product) => (product.id === id ? { ...product, price: luckyPrice } : product)),
+				};
+			}
+			return state;
+		}
+
+		case 'SUGGEST_PRODUCT_ITEM': {
+			if (lastSelectedProductId) {
+				const suggest = products.find(
+					({ id, currentCartItemQuantity }) => id !== lastSelectedProductId && currentCartItemQuantity > 0,
+				);
+				if (suggest) {
+					alert(`${suggest.name}은(는) 어떠세요? 지금 구매하시면 5% 추가 할인!`);
+					const suggestPrice = Math.round(suggest.price * 0.95);
+					return {
+						...state,
+						products: products.map((product) =>
+							product.id === suggest.id ? { ...product, price: suggestPrice } : product,
+						),
+					};
+				}
+			}
+			return state;
+		}
 		default:
 			return state;
 	}
